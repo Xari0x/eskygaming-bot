be1301111461270658f96676d4d5fac06bf7ce5a
Xari0x <32109731+Xari0x@users.noreply.github.com>
2018-06-12 19:12:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -121,14 +121,13 @@ client.on('message', msg => {
 
     if (command === "room"){
         if (args[0] === "create"){
-            msg.guild.createChannel(`🔒 Room ${msg.author.username}`, "voice");
+            msg.guild.createChannel(`🔒 Room ${msg.author.username}`, "voice").setParent('456141150852612126')
             var room_msg = new Discord.RichEmbed()
                 .setColor('#8e44ad')
                 .addField("Création d'une room privée ...", `Room ${msg.author.username}`)
                 .setTimestamp()
                 .setFooter("Codé par Xari0x | Commande demandé par " + msg.author.username, "https://cdn.discordapp.com/avatars/282147518958272512/7fbeab6cdab3222d3a567b53eff434b9.png")
             msg.guild.channels.find("name", "bot").sendEmbed(room_msg)
-            msg.guild.channels.find("name", "🔒 Room " + msg.author.username).setParent('456141150852612126')
         } else if (args[0] === "delete"){
             var room_msg = new Discord.RichEmbed()
                 .setColor('#8e44ad')
