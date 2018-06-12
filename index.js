a1b4b6f725e9f6ec7fa9ae31f5c86f12f5aa54cf
Xari0x <32109731+Xari0x@users.noreply.github.com>
2018-06-12 20:07:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -773,12 +773,7 @@ client.on('message', msg => {
 // Fonction
 function categorieroom(room) {
     if (room === null){
-        var error_msg = new Discord.RichEmbed()
-                    .setColor('#8e44ad')
-                    .addField("ERREUR", "Room === NULL")
-                    .setTimestamp()
-                    .setFooter("Codé par Xari0x", "https://cdn.discordapp.com/avatars/282147518958272512/7fbeab6cdab3222d3a567b53eff434b9.png")
-        msg.guild.channels.find("name", "bot").sendEmbed(error_msg)
+        
     } else {
         room.setParent('456141150852612126')
     }
